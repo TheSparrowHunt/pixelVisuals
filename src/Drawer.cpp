768c41903f657cc6ae84dbe6414bcae3739215de
TheSparrowHunt <jspar002@gold.ac.uk>
2016-12-02 10:13:11 +0000

    base work for class system

---
+++ src/Drawer.cpp
@@ -0,0 +1,14 @@
+//
+//  Drawer.cpp
+//  shortTest
+//
+//  Created by Jake Sparrow-Hunter on 02/12/2016.
+//
+//
+
+#include "Drawer.hpp"
+
+Drawer::Drawer(){
+    
+}
+
---
+++ src/Drawer.hpp
@@ -0,0 +1,19 @@
+//
+//  Drawer.hpp
+//  shortTest
+//
+//  Created by Jake Sparrow-Hunter on 02/12/2016.
+//
+//
+
+#ifndef Drawer_hpp
+#define Drawer_hpp
+
+#include <stdio.h>
+
+class Drawer{
+public:
+    Drawer();
+};
+
+#endif /* Drawer_hpp */
